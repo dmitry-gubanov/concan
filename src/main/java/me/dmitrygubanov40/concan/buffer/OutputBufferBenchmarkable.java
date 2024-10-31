87e0fe8dd3442a7199ad78fe5f0ecb3082258b88
Dmitry Gubanov <dmitry.gubanov40@gmail.com>
2024-10-31 23:52:37 +0300

    Simple Benchmarkable-extention for OutputBuffer.

---
+++ src/main/java/me/dmitrygubanov40/concan/buffer/OutputBufferBenchmarkable.java
@@ -0,0 +1,73 @@
+package me.dmitrygubanov40.concan.buffer;
+
+import java.util.Random;
+
+import me.dmitrygubanov40.concan.strain.Benchmarkable;
+
+
+
+/**
+ * Build-in console performance tester.
+ * @author Dmitry Gubanov, dmitry.gubanov40@gmail.com
+ */
+public class OutputBufferBenchmarkable extends OutputBuffer implements Benchmarkable
+{
+    
+    public OutputBufferBenchmarkable(final int initSize,
+                                     final boolean isSafeAsync,
+                                     final boolean autoFlushMode,
+                                     final boolean strictSizeControlMode) {
+        super(initSize, isSafeAsync, autoFlushMode, strictSizeControlMode);
+    }
+    
+    
+    
+    /**
+     * What we are going to measure with 'Benchmarkable'.
+     */
+    @Override
+    public void doBecnhmark() {
+        final int minStrLength = 0;
+        final int maxStrLength = 80;
+        //
+        for ( int i = 0; i < 100000; i++ ) {
+            String outStr = this.getBenchmarkStr(minStrLength, maxStrLength, i);
+            this.add(outStr);
+        }
+    }
+    
+    //////////
+    
+    /**
+     * Get random console string just for system load purposes.
+     * @param minLength string min length
+     * @param maxLength string max length
+     * @param randomParam randomizer for chars
+     * @return 
+     */
+    public String getBenchmarkStr(final int minLength, final int maxLength, final int randomParam) {
+        Random rnd = new Random(System.currentTimeMillis() + (long) randomParam);
+        final int strLength = rnd.nextInt(minLength, maxLength);
+        final String CHARS =     " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                + "abcdefghijklmnopqrstuvwxyz"
+                                + "1234567890"
+                                + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+                                + "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+                                + "1234567890"// yes, numbers are given twice
+                                + "!#$%&'()-@^_`{}~"
+                                + "\"*+,/:;<=>?\\[]|";
+        //
+        StringBuilder salt = new StringBuilder();
+        //
+        while ( salt.length() < strLength ) {
+            int index = (int) (rnd.nextFloat() * CHARS.length());
+            salt.append(CHARS.charAt(index));
+        }
+        //
+        String saltStr = salt.toString();
+        return saltStr;
+    }
+    
+    
+    
+}
