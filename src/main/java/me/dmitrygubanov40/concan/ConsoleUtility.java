f5f2aa7c2fe087aa10b7f2349c4c2898a1c0bd85
Dmitry Gubanov <dmitry.gubanov40@gmail.com>
2024-11-02 17:50:08 +0300

    New version of ConsoleUtility before Esc-integrations

--- src/main/java/me/dmitrygubanov40/concan/ConsoleUtility.java
+++ src/main/java/me/dmitrygubanov40/concan/ConsoleUtility.java
@@ -1,19 +1,61 @@
 package me.dmitrygubanov40.concan;
 
+
 import java.util.HashMap;
 import java.util.Set;
+
+import me.dmitrygubanov40.concan.buffer.OutputBuffer;
 
 
 
 public class ConsoleUtility
 {
     
-    private HashMap<String, Character> charAsciiCodes;
+    private HashMap<String, Character> charAsciiCodes;// ASCII-codes table
+    
+    // Whether to use rapid output of all buffer or output each symbol or command.
+    private final OutputBuffer buffer;
+    
+    
     
     ////////////////////
     
+    /**
+     * Default (empty) constructor for direct console output.
+     * Operating with standard 'println', or do not need at all.
+     */
     public ConsoleUtility() {
+        this.buffer = null;
+        //
         this.initCharAsciiCodes();
+    }
+    
+    /**
+     * To send characters right ti the buffer.
+     * @param initBuffer buffer the utility will work with
+     * @throws NullPointerException when there is no real buffer to use
+     */
+    public ConsoleUtility(final OutputBuffer initBuffer) throws NullPointerException {
+        //
+        if ( null == initBuffer ) {
+            String excMsg = "Cannot initialize buffer for Console Utility";
+            throw new NullPointerException(excMsg);
+        }
+        //
+        this.buffer = initBuffer;
+        //
+        this.initCharAsciiCodes();
+    }
+    
+    ////////////////////
+    
+    
+    
+    /**
+     * @return do we currently use the buffer?
+     */
+    public boolean isBuffering() {
+        return (null != this.buffer);
     }
     
     
@@ -32,13 +74,13 @@ public class ConsoleUtility
         this.charAsciiCodes.put("FF",  (char) 0x0C);// "Formfeed", printers' page breaker (if supported)
         this.charAsciiCodes.put("CR",  (char) 0x0D);// Carriage return
         this.charAsciiCodes.put("ESC", (char) 0x1B);// Escape character
-        this.charAsciiCodes.put("DEL", (char) 0x7F);// "Delete" (empty) no-character
+        this.charAsciiCodes.put("DEL", (char) 0x7F);// "Delete" (empty) no-character (if supported)
     }
     
     /**
      * @param charName codename of character from ASCII table (charAsciiCodes)
      * @return ASCII character as String to use in commands
-     * @throws IllegalArgumentException 
+     * @throws IllegalArgumentException if there is no such ASCII code
      */
     private String getStrCharByName(final String charName) throws IllegalArgumentException {
         if ( !this.charAsciiCodes.containsKey(charName) ) {
@@ -53,76 +95,128 @@ public class ConsoleUtility
         return controlChar.toString();
     }
     
+    
+    
     /**
-     * Base wrapper for sending direct ACSII operations.
+     * Simple output (when buffer is not used).
+     * @param charStr text to be simply printed in console
+     */
+    private void output(final String charStr) {
+        //
+        System.out.print(charStr);
+        //
+    }
+    
+    
+    
+    /**
+     * Base wrapper for sending direct ASCII operations.
      * @param charName codename of character from ASCII table (charAsciiCodes)
      * @param iterations how many times the command symbol will be sent
      * @throws IllegalArgumentException 
      */
-    private void sendCommandChar(final String charName, final int iterations) throws IllegalArgumentException {
+    private void sendAsciiChar(final String charName, final int iterations) throws IllegalArgumentException {
         if ( iterations <= 0 ) {
             String excMsg = "Number of iteraions for '" + charName + "'-char must be positive,"
                                + " but is '" + iterations + "'";
             throw new IllegalArgumentException(excMsg);
         }
         for ( int i = 0; i < iterations; i++ ) {
-            System.out.print(this.getStrCharByName(charName));
+            String charStr = this.getStrCharByName(charName);
+            if ( this.isBuffering() ) {
+                // add to a buffer
+                this.buffer.add(charStr);
+            } else {
+                // direct output of the symbol
+                this.output(charStr);
+            }
         }
     }
     
     
     
     /**
+     * "To the beginning of the line"
      * Send "carriage-return" command
      * (the very beginning of the line), \r.
      */
-    public void CR() {
-        this.sendCommandChar("CR", 1);
+    public void sendCR() {
+        this.sendAsciiChar("CR", 1);
+    }
+    public String getCR() {
+        return this.getStrCharByName("CR");
     }
     
     /**
+     * "One symbol back"
      * Send "BackSpace" command (move one symbol back), \b.
      * @param steps number of chars to left from the cursor
      */
-    public void BS(final int steps) throws IllegalArgumentException {
-        this.sendCommandChar("BS", steps);
+    public void sendBS(final int steps) {
+        this.sendAsciiChar("BS", steps);
     }
-    public void BS() {
-        this.BS(1);
+    public void sendBS() {
+        this.sendBS(1);
+    }
+    public String getBS() {
+        return this.getStrCharByName("BS");
     }
     
     /**
+     * "New line"
      * Move to new line, cursor is at the beginning, \n.
      * @param lines number of lines to move down
      */
-    public void LF(final int lines) {
-        this.sendCommandChar("LF", lines);
+    public void sendLF(final int lines) {
+        this.sendAsciiChar("LF", lines);
     }
-    public void LF() {
-        this.LF(1);
+    public void sendLF() {
+        this.sendLF(1);
+    }
+    public String getLF() {
+        return this.getStrCharByName("LF");
     }
     
     /**
-     * Make one 8-characters tabulation step to the right, \t.
+     * "Horizontal tabulation"
+     * Make one 8-characters tabulation step to the right, \t. 
      * At the end of line the output will continue at the beginning of new line.
      * @param columns number of 8-symbol steps to move right
      */
-    public void HT(final int columns) {
-        this.sendCommandChar("HT", columns);
+    public void sendHT(final int columns) {
+        this.sendAsciiChar("HT", columns);
     }
-    public void HT() {
-        this.HT(1);
+    public void sendHT() {
+        this.sendHT(1);
+    }
+    public String getHT() {
+        return this.getStrCharByName("HT");
     }
     
     /**
+     * "Vertical tabulation"
      * Keep current horizontal cursor position and then move down, \v.
      * @param lines number of lines to move down
      */
-    public void VT(final int lines) {
-        this.sendCommandChar("VT", lines);
+    public void sendVT(final int lines) {
+        this.sendAsciiChar("VT", lines);
     }
-    public void VT() {
-        this.VT(1);
+    public void sendVT() {
+        this.sendVT(1);
+    }
+    public String getVT() {
+        return this.getStrCharByName("VT");
+    }
+    
+    /**
+     * "Escape character"
+     * Essential part for escape sequences.
+     */
+    public void sendESC() {
+        this.sendAsciiChar("ESC", 1);
+    }
+    public String getESC() {
+        return this.getStrCharByName("ESC");
     }
     
     
