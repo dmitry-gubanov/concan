8f2a1f6bf680a6bd6a8a141000b5e85f531688f1
Dmitry Gubanov <dmitry.gubanov40@gmail.com>
2024-12-06 00:54:21 +0300

    Added static methods to get min and max coordinates from list of points

--- src/main/java/me/dmitrygubanov40/concan/utility/ConCord.java
+++ src/main/java/me/dmitrygubanov40/concan/utility/ConCord.java
@@ -24,6 +24,65 @@ public class ConCord
     }
     
     
+    
+    /**
+     * Pass through coordinates to search for minimal X and Y.
+     * Minimal X or Y could be taken from different points:
+     * (1, 10) and (3, 7) -> (1, 7)
+     * @param coordsToCheck array of coordinates to check
+     * @return point with minimal X and Y
+     * @throws IllegalArgumentException when less than two arguments
+     */
+    public static ConCord getMin(ConCord... coordsToCheck)
+                            throws IllegalArgumentException {
+        if ( coordsToCheck.length <= 1 ) {
+            String excMsg = "Must compare at least two points. Arguments passed: "
+                                + coordsToCheck.length;
+            throw new IllegalArgumentException(excMsg);
+        }
+        //
+        int minX = coordsToCheck[0].getX();
+        int minY = coordsToCheck[0].getY();
+        //
+        for ( int i = 1; i < coordsToCheck.length; i++ ) {
+            minX = (coordsToCheck[ i ].getX() < minX) ? coordsToCheck[ i ].getX() : minX;
+            minY = (coordsToCheck[ i ].getY() < minY) ? coordsToCheck[ i ].getY() : minY;
+        }
+        //
+        return new ConCord(minX, minY);
+    }
+    
+    /**
+     * Pass through coordinates to search for maximal X and Y.
+     * Maximal X or Y could be taken from different points:
+     * (1, 10) and (3, 7) -> (3, 10)
+     * @param coordsToCheck array of coordinates to check
+     * @return point with maximal X and Y
+     * @throws IllegalArgumentException when less than two arguments
+     */
+    public static ConCord getMax(ConCord... coordsToCheck)
+                            throws IllegalArgumentException {
+        if ( coordsToCheck.length <= 1 ) {
+            String excMsg = "Must compare at least two points. Arguments passed: "
+                                + coordsToCheck.length;
+            throw new IllegalArgumentException(excMsg);
+        }
+        //
+        int maxX = coordsToCheck[0].getX();
+        int maxY = coordsToCheck[0].getY();
+        //
+        for ( int i = 1; i < coordsToCheck.length; i++ ) {
+            maxX = (coordsToCheck[ i ].getX() > maxX) ? coordsToCheck[ i ].getX() : maxX;
+            maxY = (coordsToCheck[ i ].getY() > maxY) ? coordsToCheck[ i ].getY() : maxY;
+        }
+        //
+        return new ConCord(maxX, maxY);
+    }
+    
+    
+    //////////
+    
+    
     // actual console coordinates of the point
     private int X;
     private int Y;
