f9bbd2e979e1d56215893329ba23bd2cd90c1208
Dmitry Gubanov <dmitry.gubanov40@gmail.com>
2025-01-24 16:13:16 +0300

    Added off-mode for the archive when it is an onject but does nothing and almost does not use memeory; added max size of storage

--- src/main/java/me/dmitrygubanov40/concan/windows/ConWinOutStorage.java
+++ src/main/java/me/dmitrygubanov40/concan/windows/ConWinOutStorage.java
@@ -17,6 +17,8 @@ class ConWinOutStorage
     
     // how many symbols will be in memory by default
     private final static int DEFAULT_SIZE_LIMIT;
+    // maximum of such symbols in archive
+    private final static int MAX_SIZE_LIMIT;
     
     // start and end of command symbols in archive
     private final static char CMD_START_FLAG;
@@ -24,6 +26,7 @@ class ConWinOutStorage
     
     static {
         DEFAULT_SIZE_LIMIT = 80 * 25 * 100;
+        MAX_SIZE_LIMIT = 80 * 25 * 1000;
         //
         CMD_START_FLAG = ConUt.STX.charAt(0);
         CMD_END_FLAG = ConUt.ETX.charAt(0);
@@ -32,6 +35,9 @@ class ConWinOutStorage
     
     ///////////////////////
     
+    
+    // Is the storage operational?
+    private boolean isOff;
     
     // all characters passed by console output saved in
     // this archive (list)
@@ -57,11 +63,14 @@ class ConWinOutStorage
      */
     public ConWinOutStorage(final int initWidth, final int initLimit)
                         throws IllegalArgumentException {
-        if ( initWidth <= 0 || initLimit <= 0 ) {
+        if ( initWidth <= 0 || initLimit <= 0
+                || initLimit > ConWinOutStorage.MAX_SIZE_LIMIT ) {
             String excMsg = "Cannot initialize window storage:"
                         + " width: " + initWidth + ", size limit: " + initLimit;
             throw new IllegalArgumentException(excMsg);
         }
+        //
+        this.isOff = false;// start ready to work
         //
         this.savedOutput = new ArrayList<>();
         //
@@ -75,6 +84,34 @@ class ConWinOutStorage
         this(initWidth, DEFAULT_SIZE_LIMIT);
     }
     
+    /**
+     * Make the archive (storage) to be an empty dead-end.
+     */
+    public void turnOff() {
+        this.isOff = true;
+        this.savedOutput = null;
+        this.savedOutputLines = null;
+    }
+    
+    /**
+     * Builtin 'isOff' filter.
+     * For class-private purposes use 'generateException'
+     * @return 'true' if can work
+     * @throws IllegalStateException in case 'isOff' is 'true'
+     */
+    private boolean checkOffStatus(final boolean generateException) 
+                                throws IllegalStateException {
+        if ( this.isOff && generateException ) {
+            String excMsg = "Window output storage is 'off' but method is called";
+            throw new IllegalStateException(excMsg);
+        }
+        //
+        return this.isOff;
+    }
+    private boolean checkOffStatus() {
+        // by default do not generate an exception
+        return this.checkOffStatus(false);
+    }
     
     
     /**
@@ -83,6 +120,8 @@ class ConWinOutStorage
      * @param toCutOff how many symbols must be cut off (both 'savedOutput' and 'savedOutputLines')
      */
     private void reduceStorageData(final int toCutOff) {
+        this.checkOffStatus(true);
+        //
         if ( toCutOff <= 0 ) return;
         //
         // erase the number of chars from single-line storage
@@ -147,6 +186,8 @@ class ConWinOutStorage
      */
     public void saveOutput(final String addedTxt, final int addedTxtWidth)
                     throws NullPointerException, RuntimeException {
+        if ( this.checkOffStatus() ) return;
+        //
         if ( null == addedTxt ) {
             String excMsg = "Tried to save null-pointer string as console output";
             throw new NullPointerException(excMsg);
@@ -190,6 +231,8 @@ class ConWinOutStorage
      */
     public void saveOutputCmd(final String addedTxt)
                             throws NullPointerException {
+        if ( this.checkOffStatus() ) return;
+        //
         if ( null == addedTxt ) {
             String excMsg = "Tried to save null-pointer"
                                 + " command string as console output";
@@ -197,7 +240,8 @@ class ConWinOutStorage
         }
         //
         final String cmdToSave;
-        if ( addedTxt.length() > 0 ) {
+        final int SPECIAL_CHAR_LENGTH = 1;
+        if ( addedTxt.length() > SPECIAL_CHAR_LENGTH ) {
             // long commands (ESC-sequence)
             cmdToSave = ConWinOutStorage.CMD_START_FLAG
                             + addedTxt
@@ -216,6 +260,8 @@ class ConWinOutStorage
      * Insert (start) new line in the lines archive.
      */
     public void storeNewLine() {
+        if ( this.checkOffStatus() ) return;
+        //
         this.savedOutputLines.add(new StringBuilder());
     }
     
@@ -226,6 +272,8 @@ class ConWinOutStorage
      */
     public void deleteFirstLine()
                     throws IndexOutOfBoundsException {
+        if ( this.checkOffStatus() ) return;
+        //
         if ( this.savedOutputLines.size() <= 1 ) {
             String excMsg = "Cannot delete the last line in the storage";
             throw new IndexOutOfBoundsException(excMsg);
@@ -243,6 +291,8 @@ class ConWinOutStorage
      * Delete all empty first lines in the storage.
      */
     private void deleteFirstEmptyLines() {
+        this.checkOffStatus(true);
+        //
         while ( this.savedOutputLines.get(0).length() <= 0 ) {
             // if some first storage rows are empty - just delete them
             try {
@@ -267,8 +317,10 @@ class ConWinOutStorage
      * @return array of all symbols passed through output
      * @throws RuntimeException if symbols' archive cannot be processed correctly
      */
-    public char[] getSavedOutput(ArrayList<Character> outputLine)
+    private char[] getSavedOutput(ArrayList<Character> outputLine)
                         throws RuntimeException {
+        this.checkOffStatus(true);
+        //
         ArrayList<Character> outputToGet = new ArrayList<>(outputLine);
         // remove inner 'ConWinOutStorage' func-symbols:
         outputToGet.removeIf(
@@ -302,6 +354,9 @@ class ConWinOutStorage
      * @return string with all symbols passed through output
      */
     public String getSavedOutputStr() {
+        // an exception will rise if we want to read data from off-archive
+        this.checkOffStatus(true);
+        //
         return String.valueOf( this.getSavedOutput(this.savedOutput) );
     }
     
@@ -309,9 +364,19 @@ class ConWinOutStorage
      * @return list of all saved lines as separate Strings
      */
     public ArrayList<String> getSavedOutputLines() {
+        // an exception will rise if we want to read data from off-archive
+        this.checkOffStatus(true);
+        //
         ArrayList<String> linesResult = new ArrayList<>();
         //
         for ( StringBuilder curLine : this.savedOutputLines ) {
+            // Check new-line characters at endings.
+            // They must be removed as in 'savedOutputLines' we control new lines ourselves.
+            // Also, any LF-char ('\n') must be the last.
+            while ( curLine.indexOf(ConUt.LF) != -1 ) {
+                final int s = curLine.indexOf(ConUt.LF);
+                curLine.deleteCharAt(s);
+            }
             // get chars of current line:
             char[] curLineChars = new char[ curLine.length() ];
             curLine.getChars(0, curLine.length(), curLineChars, 0);
