2093e42b17a1070662e8418ddeba79470016cbac
Dmitry Gubanov <dmitry.gubanov40@gmail.com>
2025-05-06 23:28:38 +0300

    Added mechanism for permanent pointing to the current Terminal default color (covered 'null' value)

--- src/main/java/me/dmitrygubanov40/concan/paint/ConDrawFill.java
+++ src/main/java/me/dmitrygubanov40/concan/paint/ConDrawFill.java
@@ -19,25 +19,25 @@ import me.dmitrygubanov40.concan.utility.Term;
 public class ConDrawFill
 {
     
-    // base color from start
-    private static final Color DEFAULT_COLOR;
     // what we put as a texture for the brush
     private static final String DEFAULT_BRUSH;
-    // brush color
-    private static final Color DEFAULT_BRUSH_COLOR;
-    
     // the only length of brush string
     private static final int BRUSH_LENGTH_ALLOWED;
     
     
     static {
-        DEFAULT_COLOR = Term.get().background();
         DEFAULT_BRUSH = Term.EMPTY_CHAR;
-        DEFAULT_BRUSH_COLOR = Term.get().color();
         BRUSH_LENGTH_ALLOWED = 1;
     }
     
     //////////
+    
+    /**
+     * Both pairs 'trueColor'/'color' and 'brushTrueColor'/'brushColor'
+     * can be 'null'.
+     * That means not to have it own color, and to use Terminal colors
+     * (which can vary easy).
+     */
     
     // true color base filling color
     private Color trueColor;
@@ -70,10 +70,10 @@ public class ConDrawFill
         this.init(initColor, initBrush, initBrushColor);
     }
     public ConDrawFill(final ConCol initColor, final ConCol initBrushColor) {
-        this.init(initColor.getTrueColor(), DEFAULT_BRUSH, initBrushColor.getTrueColor());
+        this.init(initColor.getTrueColor(), ConDrawFill.DEFAULT_BRUSH, initBrushColor.getTrueColor());
     }
     public ConDrawFill(final ConCol initColor) {
-        this.init(initColor.getTrueColor(), DEFAULT_BRUSH, DEFAULT_BRUSH_COLOR);
+        this.init(initColor.getTrueColor(), ConDrawFill.DEFAULT_BRUSH, null);
     }
     
     /**
@@ -88,17 +88,25 @@ public class ConDrawFill
         this.init(initTrueColor, initBrush, initBrushTrueColor);
     }
     public ConDrawFill(final Color initTrueColor, final Color initBrushTrueColor) {
-        this.init(initTrueColor, DEFAULT_BRUSH, initBrushTrueColor);
+        this.init(initTrueColor, ConDrawFill.DEFAULT_BRUSH, initBrushTrueColor);
     }
     public ConDrawFill(final Color initTrueColor) {
-        this.init(initTrueColor, DEFAULT_BRUSH, DEFAULT_BRUSH_COLOR);
+        this.init(initTrueColor, ConDrawFill.DEFAULT_BRUSH, null);
+    }
+    
+    /**
+     * Init only 'brush' (symbol to paint areas).
+     * @param initBrush additional texture
+     */
+    public ConDrawFill(final String initBrush) {
+        this.init( (Color) null, initBrush, (Color) null);
     }
     
     /**
      * Most simple initialization with no arguments.
      */
     public ConDrawFill() {
-        this.init(DEFAULT_COLOR, DEFAULT_BRUSH, DEFAULT_BRUSH_COLOR);
+        this.init( (Color) null, ConDrawFill.DEFAULT_BRUSH, (Color) null);
     }
     
     
@@ -152,51 +160,45 @@ public class ConDrawFill
         return result;
     }
     
-    /**
-     * @param colorToCheck
-     * @throws NullPointerException when there is no color
-     */
-    protected void checkColor(final Color colorToCheck) {
-        if ( null == colorToCheck ) {
-            String excMsg = "Color is not set";
-            throw new NullPointerException(excMsg);
-        }
-    }
-    
-    /**
-     * @param concolToCheck
-     * @throws NullPointerException when there is no console color
-     */
-    protected void checkColor(final ConCol concolToCheck) {
-        if ( null == concolToCheck ) {
-            String excMsg = "Console color is not set";
-            throw new NullPointerException(excMsg);
-        }
-    }
     
     
-    
-    /**
-     * Getters block
-     */
+    // Getters block.
+    // Analyzing possible 'null'-values
+    // (which means "use current terminal default colors").
     public ConCol getConCol() {
+        if ( null == this.color ) {
+            return Term.get().background8B();
+        }
         return this.color;
     }
     public Color getColor() {
+        if ( null == this.trueColor ) {
+            return Term.get().background();
+        }
         return this.trueColor;
     }
-    public String getBrush() {
-        return this.brush;
-    }
     public ConCol getBrushConCol() {
+        if ( null == this.brushColor ) {
+            return Term.get().color8B();
+        }
         return this.brushColor;
     }
     public Color getBrushColor() {
+        if ( null == this.brushTrueColor ) {
+            return Term.get().color();
+        }
         return this.brushTrueColor;
     }
+    
+    
+    public String getBrush() {
+        return this.brush;
+    }
+    
     public ArrayList<ConStyles> getStyles() {
         return this.styles;
     }
+    
     
     
     /**
@@ -215,32 +217,40 @@ public class ConDrawFill
     
     
     
-    /**
-     * Other setters block.
-     */
+    // Other setters block.
+    // Carefully cover possible 'null'-values.
     public void setColor(final ConCol color) {
-        checkColor(trueColor);
-        //
-        this.trueColor = color.getTrueColor();
         this.color = color;
+        this.trueColor = (null != color)
+                            ? color.getTrueColor()
+                            : null;
     }
     public void setColor(final Color trueColor) {
-        checkColor(trueColor);
-        //
         this.trueColor = trueColor;
-        this.color = ConCol.getAnalog(trueColor);
+        this.color = (null != trueColor)
+                        ? ConCol.getAnalog(trueColor)
+                        : null;
     }
+    public void setColorTerm() {
+        // Non-ambiguous rapid setter.
+        this.setColor( (Color) null );
+    }
+    
     public void setBrushColor(final ConCol brushColor) {
-        checkColor(trueColor);
-        //
-        this.brushTrueColor = brushColor.getTrueColor();
         this.brushColor = brushColor;
+        this.brushTrueColor = (null != brushColor)
+                                ? brushColor.getTrueColor()
+                                : null;
     }
     public void setBrushColor(final Color brushTrueColor) {
-        checkColor(trueColor);
-        //
         this.brushTrueColor = brushTrueColor;
-        this.brushColor = ConCol.getAnalog(brushTrueColor);
+        this.brushColor = (null != brushTrueColor)
+                            ? ConCol.getAnalog(brushTrueColor)
+                            : null;
+    }
+    public void setBrushColorTerm() {
+        // Non-ambiguous rapid setter.
+        this.setBrushColor( (Color) null );
     }
     
     
@@ -251,12 +261,17 @@ public class ConDrawFill
      * @return 'true' in case colors are limited to console constants
      */
     public boolean isConCol() {
-        boolean res;
+        boolean isEqualColor = true;
+        if ( null != this.trueColor && null != this.color ) {
+            isEqualColor = this.trueColor.equals( this.color.getTrueColor() );
+        }
         //
-        // Both main color and brush color are the same RGB in
-        // TrueColor and console constants.
-        res = this.trueColor.equals( this.color.getTrueColor() )
-                && this.brushTrueColor.equals( this.brushColor.getTrueColor() );
+        boolean isEqualBrushColor = true;
+        if ( null != this.brushTrueColor && null != this.brushColor ) {
+            isEqualBrushColor = this.brushTrueColor.equals( this.brushColor.getTrueColor() );
+        }
+        //
+        final boolean res = isEqualColor && isEqualBrushColor;
         //
         return res;
     }
